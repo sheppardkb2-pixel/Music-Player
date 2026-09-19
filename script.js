be26fdd9ee765f2680f5c252ae3953c1262f00a8
sheppardkb2-pixel <sheppardkb2@vcu.edu>
2026-09-18 20:20:23 -0400

    add script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Music Container that has all the music player elements //
 const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
@@ -9,10 +10,12 @@ const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
+// Songs array and index to keep track of the current song //
 const songs = ['Khamari - These Four Walls (Lyrics)', 'MORNING DEW (DONK)', 'YUKON'];
-let songIndex = 2;
+let songIndex = 0;
 
 
+// Load the initial song into the DOM //
 function loadSong(song) {
   title.innerText = song;
   audio.src = `music/${song}.mp3`;
@@ -21,6 +24,7 @@ function loadSong(song) {
 
 loadSong(songs[songIndex]);
 
+// Play and pause the song //
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.replace('fa-play', 'fa-pause');
@@ -33,11 +37,13 @@ function pauseSong() {
   audio.pause();
 }
 
+// Add Event listeners for play, pause, previous, and next buttons //
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
   isPlaying ? pauseSong() : playSong();
 });
 
+// Add previous and next song functions //
 function prevSong() {
   songIndex = (songIndex - 1 + songs.length) % songs.length;
   loadSong(songs[songIndex]);
@@ -53,6 +59,7 @@ function nextSong() {
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
+// Update progress bar as the song plays //
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const percent = (currentTime / duration) * 100;
@@ -61,12 +68,15 @@ function updateProgress(e) {
 
 audio.addEventListener('timeupdate', updateProgress);
 
+// Set the progress of the song when the user clicks on the progress bar //
 function setProgress(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
   audio.currentTime = (clickX / width) * audio.duration;
 }
 
+// Add event listener to the progress container to allow users to click and set the progress of the song //
 progressContainer.addEventListener('click', setProgress);
 
+// When the song ends, automatically play the next song //
 audio.addEventListener('ended', nextSong);
